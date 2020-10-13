aa64783ed18f1b29d87583162537473c1415840d
Rhianna <rhiannastuart@hotmail.co.uk>
2020-10-14 00:40:37 +0100

    commited

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,5 +1,13 @@
-require 'DockingStation'
+require 'DockingStation.rb'
+require 'bike_spec.rb'
+require 'Bike_spec.rb'
 
-RSpec.describe DockingStation do
 
+
+describe DockingStation do 
+    it {is_expected.to respond_to :release_bike}
 end
+describe Bike do
+    it { is_expected.to respond_to :working? }
+
+    end
